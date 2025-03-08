4af2e4705d353f44534929ea082d55f21e9f58c8
renblnk <sierrahinson30@gmail.com>
2025-03-08 16:41:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    title = 
-    return render_template("index.html")
+    title = "My Flask Blog"
+    
+    # List of blog posts (each post is a dictionary)
+    blog_posts = [
+        {"title": "My First Blog Post", "author": "Sierra", "content": "This is my first post using Flask!", "date": "March 5, 2025"},
+        {"title": "Learning Python", "author": "Sierra", "content": "Today, I learned about Flask and templates!", "date": "March 6, 2025"},
+        {"title": "Why I Love Coding", "author": "Sierra", "content": "Coding lets me create and automate things. It's amazing!", "date": "March 7, 2025"}
+    ]
+
+    return render_template("index.html", title=title, blog_posts=blog_posts)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
